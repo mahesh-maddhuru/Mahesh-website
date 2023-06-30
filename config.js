da6ad5c8440bd008197d945bfd725a53fee6f583
Mahesh M <mahesh21319@gmail.com>
2023-06-30 13:30:03 -0400

    updated email js creds

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
   emailjs: {
     serviceID: "service_zgn3pzg",
-    templateID: "template_0p0xp5t",
+    templateID: "template_fuqwfuj",
     userID: "user_lvJPo2T7f3piQuVteW4Em"
   }
 };
